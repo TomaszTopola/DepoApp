ea39a7f533fe605dcbf3aa9544b92793f23faf18
Tomasz Topoła <t.topola.yt@gmail.com>
2023-10-21 02:27:56 +0200

    add depo counter

--- lib/services/depo_service.dart
+++ lib/services/depo_service.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:depo_app/services/server_properties.dart';
 
@@ -15,6 +14,26 @@ class DepoService{
       result = await jsonDecode(response.body);
     }catch(err){
       return [];
+    }
+    return result;
+  }
+
+  static Future<double> countDepos(String sdm) async{
+    double result;
+    try{
+      var url = Uri.http(
+        '${ServerProperties.domain}:${ServerProperties.port}',
+        '/api/no-gdpr/depo/',
+        {//PARAMS
+          'depo_status': 'ACTIVE',
+          'sdm': sdm,
+        }
+      );
+      var response = await http.get(url);
+      List<dynamic> decoded = await jsonDecode(response.body);
+      result = decoded.length.toDouble();
+    }catch(err){
+      return -1;
     }
     return result;
   }
